89ddf5f93cfb8167bc87e13ea57b7b310a11516f
Abdulkareem Olumoh <abdulkareemolumoh@protonmail.com>
2023-10-15 01:11:30 +0100

    Router Fix

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,30 @@ import {
 } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Layout from "./pages/Layout";
-import Quiz, { loader as quizLoader } from "./pages/Quiz";
+import AnyCategoryQuiz from "./pages/AnyCategoryQuiz";
+import { anyCategoryQuizLoader } from "../src/getQuizData";
 import { ThemeProvider } from "./components/ThemeContext";
+import { Quiz } from "./pages/Quiz";
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route
-          path="quiz"
-          element={<Quiz />}
-          loader={quizLoader}
-          errorElement={<h1>Easy to deal with an error here</h1>}
-        />
+        <Route path="quiz" element={<Quiz />}>
+          <Route
+            path="anyCategoryQuiz"
+            element={<AnyCategoryQuiz />}
+            loader={anyCategoryQuizLoader}
+            errorElement={<h1>Easy to deal with an error here</h1>}
+          />
+        </Route>
       </Route>
     )
   );
 
   return (
-    <ThemeProvider >
+    <ThemeProvider>
       <div>
         <RouterProvider router={router} />
       </div>
--- src/getQuizData.jsx
+++ src/getQuizData.jsx
@@ -1,5 +1,5 @@
 export async function getQuizData() {
-  const url = "https://opentdb.com/api.php?amount=5&difficulty=easy";
+  const url = "https://opentdb.com/api.php?amount=10&difficulty=easy";
   const res = await fetch(url);
   const data = await res.json();
   //function to Randomized answers
@@ -12,4 +12,28 @@ export async function getQuizData() {
     element.answers = getRandomizedAnswers(element);
   });
   return data.results;
+}
+
+export function anyCategoryQuizLoader() {
+  return getQuizData();
+}
+
+export async function getGeneralQuizData() {
+  const url = "https://opentdb.com/api.php?amount=10&category=9";
+  const res = await fetch(url);
+  const data = await res.json();
+  //function to Randomized answers
+  function getRandomizedAnswers(items) {
+    const answers = [...items.incorrect_answers, items.correct_answer];
+    return answers.sort(() => Math.random() - 0.5);
+  }
+  // Implementation of randomization of answers
+  data.results.forEach((element) => {
+    element.answers = getRandomizedAnswers(element);
+  });
+  return data.results;
+}
+
+export function generalQuizLoader() {
+  return getGeneralQuizData();
 }
